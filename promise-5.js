e80d1334d909cc6580808f7ed66118e2720e08e8
henry <henry@ezprointl.com>
2020-12-05 00:50:20 +0800

    真正意义链式回调的实现以及示例

--- promise-5.js
+++ promise-5.js
@@ -23,7 +23,8 @@ class Promise {
       this.callbacks.push(callback);
       return;
     }
-    // 如果 then 中没有传递任何东西
+    // 如果 then 中没有写回调函数
+    // 则把当前 promise 的 value 通过调用下一个 promise 的 resolve(this.value) 传递下去
     if (!callback.onFulfilled) {
       callback.resolve(this.value);
       return;
@@ -34,6 +35,11 @@ class Promise {
 
   _resolve(value) {
 
+    // *****
+    // 如果 value 也是一个 promise 对象
+    // 那么需要在 value 的 then() 之后，执行当前 promise 的 resolve 逻辑
+    // 这样就实现了当前 promise 的 onFulfilled 回调函数的执行时机依赖于它上一个 promise 的 onFulfilled 返回值的 promise 状态
+    // 从而实现多个异步操作进行同步的有顺序的执行
     if (value && (typeof value === 'object' || typeof value === 'function')) {
       const then = value.then;
       if (typeof then === 'function') {
@@ -54,7 +60,7 @@ const mockAjax = (url, time, callback) => {
   }, 1000 * time);
 };
 
-const p = new Promise(function (resolve) {
+const p = new Promise(resolve => {
   mockAjax('getUserId', 1, result => {
     resolve(result);
   });
@@ -67,6 +73,7 @@ const pThen1 = p.then(result => {
 
 p.then(result => {
   console.log('##### result2 #####', result);
+  console.log('\n');
   return '前缀' + result;
 });
 
@@ -77,4 +84,54 @@ const pThen2 = pThen1.then(result => {
 
 pThen2.then(result => {
   console.log('##### result4 #####', result);
+});
+
+// onFulfilled 回调的返回值也是一个 promise 的真实情景模拟
+// 实现多个异步请求的同步顺序执行
+const apiHdl = (params = {}, callback) => {
+  const {url = '', time} = params;
+  setTimeout(() => {
+    switch (url) {
+      case 'getRoomList':
+        callback([
+          {roomId: 1, name: '房间A'},
+          {roomId: 2, name: '房间B'}
+        ]);
+        break;
+      case 'getRoomDetail':
+        callback({
+          roomId: 1,
+          name: '房间A',
+          location: '深圳南山',
+          capacity: 10
+        });
+        break;
+      default:
+        break;
+    }
+  }, 1000 * time);
+};
+
+const getRoomList = () => {
+  return new Promise(resolve => {
+    apiHdl({url: 'getRoomList', time: 2}, result => {
+      resolve(result);
+    });
+  });
+};
+
+const getRoomDetail = (roomId) => {
+  return new Promise(resolve => {
+    apiHdl({url: 'getRoomDetail', roomId, time: 1}, result => {
+      resolve(result);
+    });
+  });
+};
+
+getRoomList().then(roomList => {
+  console.log('##### roomList #####', roomList);
+  const room = roomList[0] || {};
+  return getRoomDetail(room.roomId || '');
+}).then(roomDetail => {
+  console.log('##### roomDetail #####', roomDetail);
 });
